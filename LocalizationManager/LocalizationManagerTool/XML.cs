1a39464f49c7a213e14b86addc146685efb01923
Swiiner <almenar.killian@hotmail.com>
2024-10-29 17:30:45 +0100

    17h30 donc on commit

--- LocalizationManager/LocalizationManagerTool/XML.cs
+++ LocalizationManager/LocalizationManagerTool/XML.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Windows;
 using System.Xml;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace LocalizationManagerTool
 {
@@ -14,13 +15,37 @@ namespace LocalizationManagerTool
 
         }
 
-        private void ExportXML()
+        public void ExportXML()
         {
+            List<Word> words = new List<Word>();
 
+            for (int i = 0; i < 3; i++)
+            {
+                Word word = new Word
+                {
+                    EnUS = "Poop",
+                    FrFR = "Caca",
+                    JaJP = "Cacaligato"
+                };
+                words.Add(word);
+            }
+
+            string filePath = "words.xml"; // Spécifiez le chemin où vous voulez enregistrer le fichier
+            SerializeToXml(words, filePath);
         }
 
+        public static void SerializeToXml(List<Word> words, string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Word>), new XmlRootAttribute("Words"));
 
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                serializer.Serialize(fileStream, words);
+            }
 
+            Console.WriteLine($"Le fichier XML a été créé : {filePath}");
+        }
     }
+
 
 }
