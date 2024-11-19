ef10cc3db284f26a252b6c2931a31726fca41284
Luc D'eurveilher <eugened974@gmail.com>
2024-11-19 15:32:59 +0100

    add cs

---
+++ LocalizationManager/LocalizationManagerTool/CS.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LocalizationManagerTool
+{
+    public partial class MainWindow
+    {
+
+        private void ExportScriptCS(string filePath)
+        {
+
+            string nameClass = "public class Localization \n" +
+                    "{\n" +
+                    "   \n" +
+                    "}";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filePath, false, Encoding.UTF8))
+                {
+                    sw.WriteLine(nameClass);
+
+                    /*
+                    // Write data
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        string rowLine = string.Join(";", row.ItemArray.Select(item => item.ToString()));
+                        sw.WriteLine(rowLine);
+                    }
+                    */
+                }
+                MessageBox.Show("Exportation réussie!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lors de l'exportation : {ex.Message}");
+            }
+
+        }
+    }
+}
+
